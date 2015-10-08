ad15c85ae81351bd8d750e48bef44af89fb194f8
Mike North <michael.l.north@gmail.com>
2015-10-08 14:45:27 -0500

    Simple examples before exercise 3

--- app/routes/org.js
+++ app/routes/org.js
@@ -1,4 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+
+  authentication: Ember.inject.service(),
+
+  setupController(controller) {
+    this._super(...arguments);
+    controller.set('records', this.get('authentication.records'));
+  },
+
+  actions: {
+    addToRecords(value) {
+      this.get('authentication.records').addObject({id: value});
+    }
+  }
 });
