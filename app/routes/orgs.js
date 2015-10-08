32ca6c096deacba84ae35aa506942a4132153f91
Mike North <michael.l.north@gmail.com>
2015-10-08 15:08:12 -0500

    Exercise 3 - Services, actions, simple object properties

--- app/routes/orgs.js
+++ app/routes/orgs.js
@@ -1,6 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+
+  favorites: Ember.inject.service(),
+  actions: {
+    favoriteClicked(org) {
+      this.get('favorites').favoriteItem(org);
+    }
+  },
   model() {
     return [{
       id: "emberjs"
