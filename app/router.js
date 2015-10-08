bfff98c4ea15aefe57e7b74bd8ae21f4f8057037
Mike North <michael.l.north@gmail.com>
2015-10-08 11:34:15 -0500

    Exercise 1

--- app/router.js
+++ app/router.js
@@ -5,6 +5,16 @@ var Router = Ember.Router.extend({
   location: config.locationType
 });
 
-Router.map(function() {});
+Router.map(function() {
+  this.route('orgs', {});  //  /orgs
+  this.route('org', {path: 'org/:id'}, function() { //org/emberjs
+    this.route('repos', {});
+    this.route('repo', {path: ':repoid'}, function() {
+      this.route('contributors', {});
+      this.route('issues', {});
+    }); // org/jquery/jquery-ui
+  });
+  this.route('notfound', {path: '*path'});
+});
 
 export default Router;
---
+++ tests/unit/routes/org/repo/contributors-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:org/repo/contributors', 'Unit | Route | org/repo/contributors', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  var route = this.subject();
+  assert.ok(route);
+});
